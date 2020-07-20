9bad28c75d3e5601247ef331dfdcbb843b502d3d
Gunvir Ranu <mail@gunvirranu.com>
2020-07-20 03:03:15 -0400

    Impl BlockGrid 2D index generation

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ pub use block_width::{BlockDim, BlockWidth};
 
 use std::marker::PhantomData;
 
+type Coords = (usize, usize);
+
 #[derive(Clone, PartialEq, Eq)]
 pub struct BlockGrid<T, B: BlockDim> {
     rows: usize,
@@ -33,6 +35,28 @@ impl<T, B: BlockDim> BlockGrid<T, B> {
 
     pub fn cols(&self) -> usize {
         self.cols
+    }
+
+    fn calc_index(&self, coords: Coords) -> usize {
+        // Get block
+        let (b_row, b_col) = self.calc_block(coords);
+        let b_ind = B::AREA * (self.row_blocks() * b_row + b_col);
+        // Offset within block
+        let (s_row, s_col) = self.calc_offset(coords);
+        let s_ind = B::WIDTH * s_row + s_col;
+        b_ind + s_ind
+    }
+
+    fn row_blocks(&self) -> usize {
+        self.rows >> B::SHIFT
+    }
+
+    fn calc_block(&self, (row, col): Coords) -> Coords {
+        (row >> B::SHIFT, col >> B::SHIFT)
+    }
+
+    fn calc_offset(&self, (row, col): Coords) -> Coords {
+        (row & B::MASK, col & B::MASK)
     }
 }
 
