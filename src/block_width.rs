806a88d7eac39471a5c6a106f3b5dcc290b7e774
Gunvir Ranu <mail@gunvirranu.com>
2020-07-20 02:23:52 -0400

    Impl type-level block dimensions
    * B/c const generics aren't a thing yet, simulate them with types
    * `BlockWidth` is a module, but kinda fakes a type namespace

---
+++ src/block_width.rs
@@ -0,0 +1,38 @@
+pub trait BlockDim {
+    const SHIFT: usize;
+
+    const WIDTH: usize = 1 << Self::SHIFT;
+    const AREA: usize = Self::WIDTH * Self::WIDTH;
+    const MASK: usize = Self::WIDTH - 1;
+}
+
+macro_rules! make_block_width [
+    ($($name: ident, $shift: literal);* $(;)*) => {
+
+        #[allow(non_snake_case)]
+        pub mod BlockWidth {
+            pub use super::{$($name,)*};
+        }
+
+        $(
+            #[derive(Copy, Clone, Debug)]
+            pub struct $name;
+
+            impl BlockDim for $name {
+                const SHIFT: usize = $shift;
+            }
+        )*
+    }
+];
+
+make_block_width![
+    U2,   1;
+    U4,   2;
+    U8,   3;
+    U16,  4;
+    U32,  5;
+    U64,  6;
+    U128, 7;
+    U256, 8;
+    U512, 9;
+];
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+mod block_width;
+
+pub use block_width::{BlockDim, BlockWidth};
