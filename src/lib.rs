d089177eb1e43293d8b223df2537cb2f4b99cbf1
Gunvir Ranu <mail@gunvirranu.com>
2020-07-20 03:24:27 -0400

    Impl BlockGrid indexing operations

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod block_width;
 pub use block_width::{BlockDim, BlockWidth};
 
 use std::marker::PhantomData;
+use std::ops::{Index, IndexMut};
 
 type Coords = (usize, usize);
 
@@ -37,6 +38,29 @@ impl<T, B: BlockDim> BlockGrid<T, B> {
         self.cols
     }
 
+    pub fn get(&self, coords: Coords) -> Option<&T> {
+        self.buf.get(self.calc_index(coords))
+    }
+
+    pub fn get_mut(&mut self, coords: Coords) -> Option<&mut T> {
+        let ind = self.calc_index(coords);
+        self.buf.get_mut(ind)
+    }
+
+    // TODO: Document unsafety
+    #[allow(clippy::missing_safety_doc)]
+    pub unsafe fn get_unchecked(&self, coords: Coords) -> &T {
+        let ind = self.calc_index(coords);
+        self.buf.get_unchecked(ind)
+    }
+
+    // TODO: Document unsafety
+    #[allow(clippy::missing_safety_doc)]
+    pub unsafe fn get_unchecked_mut(&mut self, coords: Coords) -> &mut T {
+        let ind = self.calc_index(coords);
+        self.buf.get_unchecked_mut(ind)
+    }
+
     fn calc_index(&self, coords: Coords) -> usize {
         // Get block
         let (b_row, b_col) = self.calc_block(coords);
@@ -68,6 +92,27 @@ impl<T: Clone, B: BlockDim> BlockGrid<T, B> {
             cols,
             buf: vec![elem; rows * cols],
             _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T, B: BlockDim> Index<Coords> for BlockGrid<T, B> {
+    type Output = T;
+
+    fn index(&self, coords: Coords) -> &Self::Output {
+        // TODO: Benchmark against unchecked
+        match self.get(coords) {
+            Some(x) => x,
+            None => panic!("Index out of bounds"),
+        }
+    }
+}
+
+impl<T, B: BlockDim> IndexMut<Coords> for BlockGrid<T, B> {
+    fn index_mut(&mut self, coords: Coords) -> &mut Self::Output {
+        match self.get_mut(coords) {
+            Some(x) => x,
+            None => panic!("Index out of bounds"),
         }
     }
 }
