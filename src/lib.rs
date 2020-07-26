afb4dd8d43cbcfae124d0584d618163922321877
Gunvir Ranu <mail@gunvirranu.com>
2020-07-26 03:39:16 -0400

    Impl raw vec constructor to return Result

--- src/lib.rs
+++ src/lib.rs
@@ -33,15 +33,19 @@ pub struct SubBlockMut<'a, T, B: BlockDim> {
 }
 
 impl<T, B: BlockDim> BlockGrid<T, B> {
-    pub fn from_raw_vec(rows: usize, cols: usize, elems: Vec<T>) -> Self {
-        // TODO: Maybe better error-handling?
-        assert_eq!(rows * cols, elems.len());
-        Self {
+    pub fn from_raw_vec(rows: usize, cols: usize, elems: Vec<T>) -> Result<Self, ()> {
+        if rows == 0 || cols == 0 || rows * cols != elems.len() {
+            return Err(());
+        }
+        if rows % B::WIDTH != 0 || cols % B::WIDTH != 0 {
+            return Err(());
+        }
+        Ok(Self {
             rows,
             cols,
             buf: elems,
             _phantom: PhantomData,
-        }
+        })
     }
 
     pub fn take_raw_vec(self) -> Vec<T> {
@@ -267,22 +271,34 @@ impl<'a, T, B: BlockDim> IndexMut<Coords> for SubBlockMut<'a, T, B> {
 
 mod tests {
     type T = usize;
+    // TODO: Look into testing generically in a not-ugly way
     type B = super::BlockWidth::U2;
     type BlockGrid = super::BlockGrid<T, B>;
 
     #[test]
     fn test_from_raw_vec() {
-        let (rows, cols) = (4, 6);
-        let data: Vec<T> = (0..(rows * cols)).collect();
-        let grid = BlockGrid::from_raw_vec(rows, cols, data.clone());
-        assert_eq!(data.len(), grid.size());
+        for &(rows, cols) in &[(2, 2), (4, 6), (2048, 8192)] {
+            let data: Vec<T> = (0..(rows * cols)).collect();
+            let grid = BlockGrid::from_raw_vec(rows, cols, data.clone()).unwrap();
+            assert_eq!(data.len(), grid.size());
+        }
     }
 
-    #[should_panic]
     #[test]
     fn test_from_raw_vec_invalid_vec() {
-        let (rows, cols) = (4, 6);
-        let data: Vec<T> = (0..(rows * cols)).collect();
-        let _grid = BlockGrid::from_raw_vec(rows + 1, cols + 1, data);
+        for &(rows, cols) in &[(2, 2), (4, 6), (2048, 8192)] {
+            let data: Vec<T> = (0..(rows * cols)).collect();
+            let grid = BlockGrid::from_raw_vec(rows + 1, cols + 1, data);
+            assert!(grid.is_err());
+        }
+    }
+
+    #[test]
+    fn test_from_raw_vec_invalid_len() {
+        for &(rows, cols) in &[(0, 0), (0, 1), (3, 5), (7, 13)] {
+            let data: Vec<T> = (0..(rows * cols)).collect();
+            let grid = BlockGrid::from_raw_vec(rows, cols, data);
+            assert!(grid.is_err());
+        }
     }
 }
