9605b20deaad767ba27eedf7de90f1b2b9fdb560
Gunvir Ranu <mail@gunvirranu.com>
2020-08-27 00:19:00 -0400

    Bench block, each, and row major iterators

--- tb-suite/benches/unit.rs
+++ tb-suite/benches/unit.rs
@@ -81,5 +81,53 @@ fn indexing(c: &mut Criterion) {
     });
 }
 
-criterion_group!(bench, constructors, indexing);
+fn iterators(c: &mut Criterion) {
+    type B = U4;
+    const DIMS: Coords = (128, 64);
+    let data: Vec<_> = (0..(DIMS.0 * DIMS.1)).map(|x| x as u32).collect();
+    let grid = BlockGrid::<_, B>::from_raw_vec(DIMS.0, DIMS.1, data).unwrap();
+
+    let mut g = c.benchmark_group("Iterators");
+    g.bench_function("each_iter", |b| {
+        b.iter(|| {
+            for x in grid.each_iter() {
+                black_box(x);
+            }
+        })
+    });
+
+    g.bench_function("block_iter_index", |b| {
+        b.iter(|| {
+            for block in grid.block_iter() {
+                for i in 0..B::WIDTH {
+                    for j in 0..B::WIDTH {
+                        black_box(block[(i, j)]);
+                    }
+                }
+            }
+        })
+    });
+
+    g.bench_function("block_iter_get_unchecked", |b| {
+        b.iter(|| {
+            for block in grid.block_iter() {
+                for i in 0..B::WIDTH {
+                    for j in 0..B::WIDTH {
+                        black_box(unsafe { block.get_unchecked((i, j)) });
+                    }
+                }
+            }
+        })
+    });
+
+    g.bench_function("row_major_iter", |b| {
+        b.iter(|| {
+            for (c, x) in grid.row_major_iter() {
+                black_box((c, x));
+            }
+        })
+    });
+}
+
+criterion_group!(bench, constructors, indexing, iterators);
 criterion_main!(bench);
