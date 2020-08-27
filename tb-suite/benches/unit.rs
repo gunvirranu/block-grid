3f02a510092b53ac317a46a46e47375dab13d084
Gunvir Ranu <mail@gunvirranu.com>
2020-08-26 22:53:48 -0400

    Bench row and col major constructors

--- tb-suite/benches/unit.rs
+++ tb-suite/benches/unit.rs
@@ -1,11 +1,37 @@
 extern crate block_grid;
 extern crate criterion;
 
-use criterion::{criterion_group, criterion_main, Criterion};
+use block_grid::{BlockWidth::*, *};
+use criterion::{criterion_group, criterion_main, Bencher, Criterion};
 
-// TODO: Add tests
+fn constructors(c: &mut Criterion) {
+    const DIMS: Coords = (128, 256);
 
-fn empty(_c: &mut Criterion) {}
+    fn gen_data_u8(len: usize) -> Vec<u8> {
+        (0..len).map(|x| x as u8).collect()
+    }
 
-criterion_group!(bench, empty);
+    fn bench_from_row_major<B: BlockDim>() -> impl Fn(&mut Bencher, &Coords) {
+        |b, &(rows, cols)| {
+            assert!(rows % B::WIDTH == 0 && cols % B::WIDTH == 0);
+            let data = gen_data_u8(rows * cols);
+            b.iter_with_large_drop(|| BlockGrid::<_, B>::from_row_major(rows, cols, &data))
+        }
+    }
+    let mut g = c.benchmark_group("Constructors");
+    g.bench_with_input("from_row_major<U2>", &DIMS, bench_from_row_major::<U2>());
+    g.bench_with_input("from_row_major<U8>", &DIMS, bench_from_row_major::<U8>());
+
+    fn bench_from_col_major<B: BlockDim>() -> impl Fn(&mut Bencher, &Coords) {
+        |b, &(rows, cols)| {
+            let data = gen_data_u8(rows * cols);
+            b.iter_with_large_drop(|| BlockGrid::<_, B>::from_col_major(rows, cols, &data))
+        }
+    }
+    g.bench_with_input("from_col_major<U2>", &DIMS, bench_from_col_major::<U2>());
+    g.bench_with_input("from_col_major<U8>", &DIMS, bench_from_col_major::<U8>());
+    g.finish();
+}
+
+criterion_group!(bench, constructors);
 criterion_main!(bench);
