c15bccdf52c55df78aafc5910c887273c8b93eab
Gunvir Ranu <mail@gunvirranu.com>
2020-09-08 05:39:00 -0400

    Test block-grid blox

--- tb-suite/tests/blur.rs
+++ tb-suite/tests/blur.rs
@@ -34,8 +34,9 @@ fn gen_test_index<B: BlockDim>(rows: usize, cols: usize) {
 }
 
 fn gen_test_idiomatic<B: BlockDim>(rows: usize, cols: usize) {
-    let mut in_index = BlockGrid::<u8, B>::new(rows, cols).unwrap();
-    let mut out_index = in_index.clone();
+    let mut in_bg = BlockGrid::<u8, B>::new(rows, cols).unwrap();
+    let mut out_bg = in_bg.clone();
+    let mut out_index = in_bg.clone();
 
     let mut in_ar = Array2D::filled_with(0u8, rows, cols);
     let mut out_ar = in_ar.clone();
@@ -44,17 +45,19 @@ fn gen_test_idiomatic<B: BlockDim>(rows: usize, cols: usize) {
     for i in 0..rows {
         for j in 0..cols {
             let x = fastrand::u8(..);
-            in_index[(i, j)] = x;
+            in_bg[(i, j)] = x;
             in_ar[(i, j)] = x;
         }
     }
 
-    blur_by_index(rows, cols, &in_index, &mut out_index);
+    blur_by_index(rows, cols, &in_bg, &mut out_index);
+    blur_blockgrid(&in_bg, &mut out_bg);
     blur_array2d(&in_ar, &mut out_ar);
 
     for i in 0..rows {
         for j in 0..cols {
             let x = out_index[(i, j)];
+            assert_eq!(out_bg[(i, j)], x);
             assert_eq!(out_ar[(i, j)], x);
         }
     }
